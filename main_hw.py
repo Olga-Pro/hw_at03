2a799a31278260b6d6e90eb3e25b1cdece7fc4d9
Olga-Pro <oip_@inbox.ru>
2024-09-03 15:20:14 +0300

    add comments

--- main_hw.py
+++ main_hw.py
@@ -1,6 +1,10 @@
 import requests
 
 def get_random_cat_image():
+    '''
+    Функция делает запрос к TheCatAPI для получения случайного изображения кошки.
+    Возвращает URL изображения, если запрос успешен, иначе возвращает None.
+    '''
     url = 'https://api.thecatapi.com/v1/images/search'
     response = requests.get(url)
     if response.status_code == 200:
--- test_hw.py
+++ test_hw.py
@@ -3,6 +3,9 @@ from main_hw import get_random_cat_image
 
 
 def test_get_random_cat_image(mocker):
+    '''
+    Тест, который проверяет успешный запрос и возвращает правильный URL.
+    '''
     mock_get = mocker.patch('requests.get')
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = [{'url': 'https://example.com/cat.jpg'}]
@@ -13,6 +16,9 @@ def test_get_random_cat_image(mocker):
 
 
 def test_get_random_cat_image_with_error(mocker):
+    '''
+    Тест, который проверяет неуспешный запрос (например, статус код 404) и возвращает None.
+    '''
     mock_get = mocker.patch('requests.get')
     mock_get.return_value.status_code = 404
 
